83f34c3c0fc40259260cf67ac494190681e704f0
ijwilliams <isaac@thewilliams.ws>
2015-10-22 10:52:15 -0400

    Actual sieve

--- sieve/sieve.rb
+++ sieve/sieve.rb
@@ -4,14 +4,21 @@ class Sieve
   end
 
   def primes
-    @range.select { |n| prime?(n) }
+    return @primes unless @primes.nil?
+    blacklist = composites(@range)
+    @primes = @range.select { |p| !blacklist.include?(p) }
   end
 
   private
 
-  def prime?(n)
-    return false if n == 1
-    return true if n == 2
-    (2..Math.sqrt(n).ceil).none? { |num| n % num == 0 }
+  def composites(range)
+    c = []
+    (2..Math.sqrt(range.last)).each do |n|
+      if n**2 < range.last
+        c << (n**2..range.last).select { |b| b % n == 0 }
+        c.flatten!
+      end
+    end
+    c
   end
 end
