ae4fbf5809dc6215c72a89ae058c0fc0f78b95cf
ijwilliams <isaac@thewilliams.ws>
2015-10-27 07:40:27 -0400

    Calculate primes directly
    to_a is super-expensive on big ranges. I want to write an iteration
    that doesn't use it....

--- sieve/sieve.rb
+++ sieve/sieve.rb
@@ -4,21 +4,12 @@ class Sieve
   end
 
   def primes
-    return @primes unless @primes.nil?
-    blacklist = composites(@range)
-    @primes = @range.select { |p| !blacklist.include?(p) }
-  end
-
-  private
-
-  def composites(range)
-    c = []
-    (2..Math.sqrt(range.last)).each do |n|
-      if n**2 < range.last
-        c << (n**2..range.last).select { |b| b % n == 0 }
-        c.flatten!
+    @primes = @range.to_a
+    (2..Math.sqrt(@range.last)).each do |n|
+      if n**2 < @range.last
+        @primes = @primes - (n**2..@range.last).select { |b| b % n == 0 }
       end
     end
-    c
+    @primes
   end
 end
