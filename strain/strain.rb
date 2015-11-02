1b38926d5c1020fc9397e8a20ecd363b16e0608d
ijwilliams <isaac@thewilliams.ws>
2015-11-02 13:52:45 -0500

    Remove self references and implement the glorious each_with_object

--- strain/strain.rb
+++ strain/strain.rb
@@ -2,15 +2,13 @@ module Enumerable
   def keep
     # could have been a one line select statement, but they told
     # me to "solve this one [myself] using other basic tools"
-    array = []
-    self.each do |n|
-      array << n if yield(n)
+    each_with_object([]) do |x, r|
+      r << x if yield(x)
     end
-    array
   end
 
   def discard
-    kept = self.keep(&Proc.new)
+    kept = keep(&Proc.new)
     self - kept
   end
 end
