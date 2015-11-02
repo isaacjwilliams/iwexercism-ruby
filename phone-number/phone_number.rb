6301baf76b9a2b07db9150983c188f077e8d84d1
ijwilliams <isaac@thewilliams.ws>
2015-11-02 13:52:20 -0500

    Optimize PhoneNumber

--- phone-number/phone_number.rb
+++ phone-number/phone_number.rb
@@ -1,31 +1,30 @@
 class PhoneNumber
-  def initialize(phone_number)
-    @phone_number = phone_number.chars.select do |c|
+  @@default_number = '0000000000'
+  attr_reader :number
+
+  def initialize(number)
+    @number = number.chars.select do |c|
       !/[^\w]/.match(c)
     end.join
-    filter_phone_number
-  end
-
-  def number
-    @phone_number
+    filter_number
   end
 
   def area_code
-    @phone_number[0..2]
+    @number[0..2]
   end
 
   def to_s
-    "(#{@phone_number[0..2]}) #{@phone_number[3..5]}-#{@phone_number[6..-1]}"
+    "(#{area_code}) #{@number[3..5]}-#{@number[6..-1]}"
   end
 
   private
 
-  def filter_phone_number
-    @phone_number = '0000000000' unless valid_content?
-    @phone_number = @phone_number.sub(/\A1/, '') if @phone_number.size == 11
+  def filter_number
+    @number = @@default_number unless valid_content?
+    @number = @number[1..-1] if @number.size == 11
   end
 
   def valid_content?
-    /\A1?[\d]{10}\Z/ =~ @phone_number
+    /\A1?[\d]{10}\Z/ =~ @number
   end
 end
