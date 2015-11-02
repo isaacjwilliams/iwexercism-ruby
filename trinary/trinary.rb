ae4178c63c3a6171e48f247787cd84c0e98cbf44
ijwilliams <isaac@thewilliams.ws>
2015-11-02 13:52:55 -0500

    Get rid of useless checks

--- trinary/trinary.rb
+++ trinary/trinary.rb
@@ -2,12 +2,10 @@ class Trinary
   VERSION = 1
 
   def initialize(trinary)
-    @trinary = trinary if trinary =~ /[0-2]+/
+    @trinary = trinary
   end
 
   def to_decimal
-    return 0 unless @trinary
-
     reversed_trinary = @trinary.chars.reverse
     reversed_trinary.each.with_index.inject(0) do |decimal, (digit, index)|
       decimal + (digit.to_i * (3**index))
